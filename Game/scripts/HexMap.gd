9904a011d8f80b50c33f1b031d6b9cb2f2871005
MrDiver <mrdiverlp@gmail.com>
2020-03-30 23:02:08 +0200

    Added a Map

---
+++ Game/scripts/HexMap.gd
@@ -0,0 +1,5 @@
+extends Spatial
+class_name HexMap
+
+func _ready():
+	pass
---
+++ Game/scripts/HexTile.gd
@@ -0,0 +1,6 @@
+extends Spatial
+
+class_name HexTile
+export(String,"Wald","Hügelland","Weideland","Ackerland","Gebirge") var TYPE
+func _ready():
+	pass
